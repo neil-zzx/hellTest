275d21831104f3177fc2e529ad356e73a85fe645
zhixiang.zhang <335902237@qq.com>
2017-10-20 09:43:50 +0800

    测试第一版

---
+++ src/main/java/com/gittest/zhph/Test.java
@@ -0,0 +1,55 @@
+package com.gittest.zhph;
+
+/***
+ * idea 测试git类
+ * @author  zhixiang.zhang
+ * @date 2017年10月20日
+ */
+public class Test {
+    private String name;
+    private  String age;
+    private   String sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "Test{" +
+                "name='" + getName() + '\'' +
+                ", age='" + getAge() + '\'' +
+                ", sex='" + getSex() + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) {
+
+        Test test = new Test();
+        test.setAge("18");
+        test.setName("小名");
+        test.setSex("男");
+        System.out.println(test);
+    }
+
+}
